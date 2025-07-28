c1cd44983fb04f65a270d8568e7748857867e511
Alexandro <cocouvialexandro74@gmail.com>
2025-07-28 08:21:48 +0100

    premier commit8

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,21 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    base: '/build/',
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
+    build: {
+        manifest: true,
+        outDir: 'public/build',
+        rollupOptions: {
+            output: {
+                manualChunks: undefined,
+            }
+        }
+    }
 });
