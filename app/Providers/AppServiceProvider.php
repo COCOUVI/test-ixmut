a4f93a72642e21aba378fcaf65d2529e2550d31b
Alexandro <cocouvialexandro74@gmail.com>
2025-07-28 12:05:50 +0100

    premier commit8

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -10,15 +10,15 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Register any application services.
      */
-    public function register()
-    {
-        if (env('APP_ENV') !== 'local') {
-            URL::forceScheme('https');
-        }
-    }
+    public function register(): void {}
 
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void {}
+    public function boot()
+    {
+        if (env('APP_ENV') === 'production') {
+            URL::forceScheme('https'); // 🔐 Force HTTPS
+        }
+    }
 }
--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,18 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
-    build: {
-        manifest: true,
-        outDir: 'public/build',
-        rollupOptions: {
-            output: {
-                manualChunks: undefined,
-            }
-        }
-    }
+  plugins: [
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.js'],
+      refresh: true,
+    }),
+  ],
+  server: {
+    https: true, // <---- ⚠️ Utile pour le dev local aussi si tu veux
+  },
+  build: {
+    manifest: true,
+    outDir: 'public/build',
+  },
 });
